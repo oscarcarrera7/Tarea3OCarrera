bc1cd26eaab128abddd630e637a412ea5dbf154c
Oscar Carrera <ocarrera_electric@hotmail.com>
2021-04-26 17:33:34 -0500

    Nuevo cambio

---
+++ Tarea3OCarrera/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea3OCarrera
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            String user = txtUser.Text;
+            String pass = txtPass.Text;
+
+            if((user == "estudiante2021") && (pass == "uisrael2021"))
+            {
+                await Navigation.PushAsync(new ventanaDos(user, pass));
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario o contraseña incorrecta", "Gracias");
+            }
+            
+        }
+    }
+}
---
+++ Tarea3OCarrera/ventanaDos.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea3OCarrera
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ventanaDos : ContentPage
+    {
+        public ventanaDos( String user, String pass)
+        {
+            InitializeComponent();
+            lblUser.Text = "Bienvenido: " + user;
+
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double dato1 = Convert.ToDouble(txtSeguimiento1.Text);
+                double dato2 = Convert.ToDouble(txtExamen1.Text);
+                double dato3 = Convert.ToDouble(txtSeguimiento2.Text);
+                double dato4 = Convert.ToDouble(txtExamen2.Text);
+
+                double suma = ((dato1 * 0.3) + (dato2 * 0.2));
+                double suma2 = ((dato3 * 0.3) + (dato4 * 0.2));
+                double suma3 = suma + suma2;
+
+                txtParcial1.Text = suma.ToString();
+                txtParcial2.Text = suma2.ToString();
+                txtNota.Text = suma3.ToString();
+
+                if (suma3 >= 7)
+                {
+                    DisplayAlert("Mensaje Final", "Aprobado", "Gracias");
+                }
+                else if (suma3 >= 5 && suma3 <= 6.9)
+                {
+                    DisplayAlert("Mensaje Final", "Complementrio", "Gracias");
+                }
+                else if (suma3 < 4.9)
+                {
+                    DisplayAlert("Mensaje Final", "Reprobado", "Gracias");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+
+        }
+    }
+}
